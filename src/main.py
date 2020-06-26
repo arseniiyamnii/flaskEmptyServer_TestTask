2bc14a8249c77f826a26486c38fb5420e0ee4f11
Arsenii Yamnii <yamnii@ya.ru>
2020-06-27 04:12:33 +0500

    ready with beautiful logging but can do this better

--- src/main.py
+++ src/main.py
@@ -2,17 +2,38 @@
 
 import os
 from flask import Flask
+from flask import request
+import time
+import logging
+import json
+
 app = Flask(__name__)
-
-
+logging.basicConfig(filename='log',format='%(asctime)-15s %(message)s', level=logging.INFO)
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 @app.route('/')
 def hello():
     return("Hello World!")
 
 @app.route('/<path:other>')
 def fallback(other):
-        return 'This one catches everything else'
+    message=""
+    if(request.method!="GET"):
+        message+="ERROR::"
+        #print(json.dumps(request.args))
+        #logging.info(request.method,request.url,request.args,time.time)
+    message+="methodSUKA="+request.method+" url="+request.url
+    if request.args:
+        message+=" args="+json.dumps(request.args)
+    else:
+        message+=" args=none"
+    logging.info(message)
+    print("method= ",request.method)
+    print("url= ",request.url)
+    print("args= ",request.args)
+    print("time= ", time.time())
+    return 'This one catches everything else'
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=os.environ.get('PORT'), debug=True)
+    app.run(host="localhost", port=os.environ.get('PORT'))
 
